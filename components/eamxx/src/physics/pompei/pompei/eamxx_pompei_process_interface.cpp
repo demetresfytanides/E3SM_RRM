c2d0605eafb775f143d9ef07638266342a3f97ff
mahf708 <naser.mahfouz@pnnl.gov>
2025-02-24 05:34:26 -0800

    EAMxx tutorial step 9: add mask in initialize_impl

--- components/eamxx/src/physics/pompei/pompei/eamxx_pompei_process_interface.cpp
+++ components/eamxx/src/physics/pompei/pompei/eamxx_pompei_process_interface.cpp
@@ -110,6 +110,66 @@ set_grids (const std::shared_ptr<const GridsManager> grids_manager)
 void POMPEI::initialize_impl (const RunType /* run_type */)
 {
   // NOTE: run_type tells us if this is an initial or restarted run,
+  // Some universal constants that we will need
+  using PC = scream::physics::Constants<Real>;
+  constexpr Real deg2rad = PC::Pi / 180;
+  constexpr Real r_earth = PC::r_earth;
+
+  // We can create our helper "mask" field. It will be 1 where the volcano injection
+  // in the atmosphere happens, and 0 elsewhere.
+  const std::string mask_name = "emission_mask";
+  constexpr auto nondim = ekat::units::Units::nondimensional();
+  const auto layout_3d = m_grid->get_3d_scalar_layout(true);
+  FieldIdentifier rate_fid(mask_name,layout_3d,nondim,m_grid->name());
+  m_emission_mask = Field(rate_fid);
+  m_emission_mask.allocate_view();
+  m_emission_mask.deep_copy(0); // 0 means "not injecting here"
+
+  // The grid already stores lat/lon, so go ahead and pre-compute the mask field
+  auto lat = m_grid->get_geometry_data("lat"); // WATCH OUT: it's in degrees, NOT rad
+  auto lon = m_grid->get_geometry_data("lon"); // WATCH OUT: it's in degrees, NOT rad
+
+  // Target location of volcanic eruption
+  auto volcano_lat = 40.8214 * deg2rad;
+  auto volcano_lon = 14.4260 * deg2rad;
+  auto emission_lev = 27;
+  auto radius = m_params.get<double>("plume_radius_in_km");
+  EKAT_REQUIRE_MSG (radius>0,
+      "Error! Plume radius should be positive. Input value: " << radius << ".\n");
+
+  // Extract Kokkos (device) views from the fields
+  // NOTE: views are "just" multi-dimensional arrays, which are accessible on device
+  auto emission_view = m_emission_mask.get_view<Real**>();
+  auto lat_view = lat.get_view<const Real*>();
+  auto lon_view = lon.get_view<const Real*>();
+
+  // ------------------------------------------------------------------------------------------- //
+  // We define a lambda function for computing the mask that we can then use with
+  // Kokkos.
+  // A lambda is just a function defined "on-the fly". KOKKOS_LAMBDA simply adds
+  // some decoration for GPU execution, nothing to worry about for now
+  auto compute_mask = KOKKOS_LAMBDA (const int icol) {
+    auto lat_rad = lat_view(icol) * deg2rad;
+    auto lon_rad = lon_view(icol) * deg2rad;
+    auto delta_lat = lat_rad - volcano_lat;
+    auto delta_lon = lon_rad - volcano_lon;
+
+    auto dist = r_earth/1e3 * sqrt(delta_lat*delta_lat + delta_lon*delta_lon);
+
+    // If the distance between this point and the center of the volcano is within the radius
+    // set the MASK value to 1 (true).
+    if (dist<radius) {
+      emission_view(icol,emission_lev) = 1;
+    }
+  };
+  // ------------------------------------------------------------------------------------------- //
+
+  // A policy tells kokkos how to parallelize the loop. Here, we are doing
+  // a single for loop over the range of indices [0,ncols)
+  auto policy = Kokkos::RangePolicy<Field::device_t::execution_space>(0,m_ncols);
+
+  // Execute the lambda in parallel according to the execution policy
+  Kokkos::parallel_for("",policy,compute_mask);
 }
 
 /*-----------------------------------------------------------------------------------------------
